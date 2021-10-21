35e90382ae9dde06630353964877250fcfe6f422
FrancoPaganucci <francoepaganucci@gmail.com>
2021-10-21 15:18:47 -0300

    modulos con funcion de pisos y planos faltan img

--- 955 belgrano office/js/index.js
+++ 955 belgrano office/js/index.js
@@ -4,6 +4,8 @@ const img_plano = document.getElementById('img-plano');
 const area_rentable = document.getElementById('area-rentable');
 const area_alfombra = document.getElementById('area-alfombra');
 const area_total = document.getElementById('area-total');
+const piso = document.getElementById('h3-piso');
+var tooltips = document.getElementsByClassName('tooltip');
 
 // pestañas
 area_rentable.addEventListener('click', () => {
@@ -11,22 +13,31 @@ area_rentable.addEventListener('click', () => {
     area_alfombra.style.color = "#000000";
     area_total.style.color = "#000000";
     img_plano.src = "./assets/955_rentable_1018.jpg";
+    superficie.innerHTML = "1018 m2";
 });
 area_alfombra.addEventListener('click', () => {
     area_alfombra.style.color = "orangered";
     area_rentable.style.color = "#000000";
     area_total.style.color = "#000000";
     img_plano.src = "./assets/955_alfombra_778.jpg";
+    superficie.innerHTML = "778 m2";
 });
 area_total.addEventListener('click', () => {
     area_total.style.color = "orangered";
     area_alfombra.style.color = "#000000";
     area_rentable.style.color = "#000000";
     img_plano.src = "./assets/955_total_1213.jpg";
+    superficie.innerHTML = "1213 m2";
 });
 
 
-// superficies por piso
-piso_prueba.addEventListener('click', () => {
-    superficie.innerHTML = "1932 m2";
-})
+// función pisos
+for (i = 0; i < tooltips.length; i++) {
+    let numero = tooltips[i].id;
+    tooltips[i].addEventListener('click', () => {
+        cambiarPiso(numero);
+    });
+};
+function cambiarPiso(numero) {
+    piso.innerHTML = "Piso " + numero;
+}
--- le parc punta del este/js/index.js
+++ le parc punta del este/js/index.js
@@ -4,6 +4,8 @@ const img_plano = document.getElementById('img-plano');
 const area_rentable = document.getElementById('area-rentable');
 const area_alfombra = document.getElementById('area-alfombra');
 const area_total = document.getElementById('area-total');
+const piso = document.getElementById('h3-piso');
+var tooltips = document.getElementsByClassName('tooltip');
 
 // pestañas
 area_rentable.addEventListener('click', () => {
@@ -11,22 +13,30 @@ area_rentable.addEventListener('click', () => {
     area_alfombra.style.color = "#000000";
     area_total.style.color = "#000000";
     img_plano.src = "./assets/MO_rentable_1234.jpg";
+    superficie.innerHTML = "1234 m2";
 });
 area_alfombra.addEventListener('click', () => {
     area_alfombra.style.color = "orangered";
     area_rentable.style.color = "#000000";
     area_total.style.color = "#000000";
     img_plano.src = "./assets/MO_alfombra_1046.jpg";
+    superficie.innerHTML = "1046 m2";
 });
 area_total.addEventListener('click', () => {
     area_total.style.color = "orangered";
     area_alfombra.style.color = "#000000";
     area_rentable.style.color = "#000000";
     img_plano.src = "./assets/MO_total_1525.jpg";
+    superficie.innerHTML = "1525 m2";
 });
 
-
-// superficies por piso
-piso_prueba.addEventListener('click', () => {
-    superficie.innerHTML = "1932 m2";
-})
+// función pisos
+for (i = 0; i < tooltips.length; i++) {
+    let numero = tooltips[i].id;
+    tooltips[i].addEventListener('click', () => {
+        cambiarPiso(numero);
+    });
+};
+function cambiarPiso(numero) {
+    piso.innerHTML = "Piso " + numero;
+}
